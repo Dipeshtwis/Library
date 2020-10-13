c9acc8f84c65dde0d33c963d926bd1a60ec30891
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-10-13 19:36:28 +0530

    Add submitted book to the array

--- js/index.js
+++ js/index.js
@@ -1,15 +1,39 @@
 let myLibrary = []
 
-function Book(name, price, page){
-
+function Book(author, title, num, status){
+	this.author = author;
+	this.title = title;
+	this.num = num;
+	this.status = status;
 }
 
-function addBookToLibrary(){
-
+function addBookToLibrary(book){
+	myLibrary.push(book);
+	displayBook(myLibrary);
 }
 
+function myFunction(){
+const author = document.getElementById("author").value;
+const title = document.getElementById("title").value;
+const num = document.getElementById("num").value;
+let status;
+if(document.getElementById("read").checked)
+	status = true;
+else
+	status = false;
 
-const btn = document.querySelector('#click');
-btn.addEventListener('click', () => {
-	alert("Hey!");
-});
+let book = new Book(author, title, num, status);
+addBookToLibrary(book);	
+alert("hey!!")
+}
+
+function displayBook(myLibrary){
+	for (var i = 0; i < myLibrary.length; i++) {
+		console.log(myLibrary[i]);
+	}
+}
+
+// const btn = document.querySelector('#click');
+// btn.addEventListener('click', () => {
+// alert("Hey!");
+// });
