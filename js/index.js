03c43ed10559a87148c983c536307345c24e1a59
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-10-13 18:48:31 +0530

    Add HTML form and btn-event

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let myLibrary = []
+
+function Book(name, price, page){
+
+}
+
+function addBookToLibrary(){
+
+}
+
+
+const btn = document.querySelector('#click');
+btn.addEventListener('click', () => {
+	alert("Hey!");
+});
