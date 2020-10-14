820a479ffdc87da03f1b97c46b027fdb94ddedc5
praz <t.prazwal@gmail.com>
2020-10-14 11:26:55 +0200

    add DOM manipulation.

--- js/index.js
+++ js/index.js
@@ -1,39 +1,59 @@
-let myLibrary = []
+let myLibrary = [];
 
 function Book(author, title, num, status){
 	this.author = author;
 	this.title = title;
 	this.num = num;
 	this.status = status;
+	this.info = `${title} written by ${author} has ${num} pages. ${status}`;
 }
 
 function addBookToLibrary(book){
 	myLibrary.push(book);
-	displayBook(myLibrary);
 }
 
-function myFunction(){
-const author = document.getElementById("author").value;
-const title = document.getElementById("title").value;
-const num = document.getElementById("num").value;
-let status;
-if(document.getElementById("read").checked)
-	status = true;
-else
-	status = false;
+// function myFunction(){
+	// const author = document.getElementById("author").value;
+	// const title = document.getElementById("title").value;
+	// const num = document.getElementById("num").value;
+	// let status;
+	// if(document.getElementById("read").checked)
+	// 	status = true;
+	// else
+	// 	status = false;
 
-let book = new Book(author, title, num, status);
-addBookToLibrary(book);	
-alert("hey!!")
-}
+	// let book = new Book(author, title, num, status);
+	// addBookToLibrary(book);
+	// displayBook(myLibrary);
+	// alert("hey!!")
+// }
 
 function displayBook(myLibrary){
-	for (var i = 0; i < myLibrary.length; i++) {
-		console.log(myLibrary[i]);
+	
+	let newBook = document.createElement('div');
+	// newBook.textContent = 'Hello';
+	body.appendChild(newBook);
+	
+	for(let i = 0; i < myLibrary.length; i++) {
+		let para = document.createElement('p');
+		para.innerHTML = `author: ${myLibrary[i].author}`;
+		newBook.appendChild(para);
 	}
 }
+const btn = document.getElementById('#click');
+if(btn){
+	btn.addEventListener('click', () => {
+		const author = document.getElementById("author").value;
+		const title = document.getElementById("title").value;
+		const num = document.getElementById("num").value;
+		let status;
+		if(document.getElementById("read").checked)
+			status = true;
+		else
+			status = false;
 
-// const btn = document.querySelector('#click');
-// btn.addEventListener('click', () => {
-// alert("Hey!");
-// });
+		let book = new Book(author, title, num, status);
+		addBookToLibrary(book);
+		displayBook(myLibrary);
+	});
+}
