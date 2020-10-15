2ce60643733f9aff74016be737fd3e328ab23781
praz <t.prazwal@gmail.com>
2020-10-15 17:24:51 +0200

    fix linter errors.

--- js/index.js
+++ js/index.js
@@ -26,24 +26,25 @@ function displayBook(myLibrary) {
   const newBook = document.getElementById('tableBody');
   let str = '';
   myLibrary.forEach((bokk, index) => {
-  	str += `
-		<tr>
-			<td> ${index + 1}</td>
-			<td> ${bokk.author}</td>
-			<td> ${bokk.title}</td>
-			<td> ${bokk.num}</td>
-			<td> <input onclick="change(this, ${index})" type="button" value="${bokk.status}" id="" class="btn btn-primary"></input></td>
-			<td> <button onclick="deleted(${index})" class="btn btn-danger">Delete</button></td>
-		</tr>
-		`;
+    str += `
+    <tr>
+      <td> ${index + 1}</td>
+      <td> ${bokk.author}</td>
+      <td> ${bokk.title}</td>
+      <td> ${bokk.num}</td>
+      <td> <input onclick="change(this, ${index})" type="button" value="${bokk.status}" id="" class="btn btn-primary"></input></td>
+      <td> <button onclick="deleted(${index})" class="btn btn-danger">Delete</button></td>
+    </tr>
+    `;
   });
   newBook.innerHTML = str;
 }
 
+/* eslint-disable no-unused-vars */
 function change(elem, index) {
   if (elem.value === 'read') {
     elem.value = 'unread';
-  }	else {
+  } else {
     elem.value = 'read';
   }
   myLibrary[index].status = elem.value;
@@ -53,7 +54,7 @@ function deleted(index) {
   myLibrary.splice(index, 1);
   displayBook(myLibrary);
 }
-
+/* eslint-enable no-unused-vars */
 
 const btn = document.getElementById('click');
 if (btn) {
