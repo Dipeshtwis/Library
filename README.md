79c9719be23cd6cc4730a292ef651ac011d012de
praz <t.prazwal@gmail.com>
2020-10-15 16:55:30 +0200

    update readme.md file

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# Libraray
+# Library
+
+> This is the first project in JavaScript module of Microverse main technical curriculum. This project is a simple mock-up of a library where we can add a new book and delete it too. While adding a new book, we are presented with a form where we can input the details of the book including title of the book, author, number of pages and the status of book indicating whether the book has been read or not.
+
+![screenshot](img/library.png)
+
+## Built With
+- JavaScript
+- HTML 5 and CSS 3.
+- Bootstrap
+
+## Live Demo
+
+[Live Demo Link](https://dipeshtwis.github.io/Library/ )
+
+## Getting Started
+
+Just fork it and you are good to go.
+
+## Authors
+
+👤 **Prajwal Thapa**
+
+- Github: [praz99](https://github.com/praz99)
+- Twitter: [@thapa_praz](https://twitter.com/thapa_praz)
+- LinkedIn: [prazwal-thapa](https://linkedin.com/in/prazwal-thapa)
+- Email: (t.prazwal@gmail.com)
+
+👤 **Dipesh Kumar** 
+- Github: [@Dipeshtwis](https://github.com/Dipeshtwis) 
+- Twitter: [@97deepeshkumar](https://twitter.com/97deepeshkumar) 
+- Linkedin: [dipeshtwis](https://www.linkedin.com/in/dipeshtwis/)
+- Email: (deepesh.97kumar@gmail.com)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the [issues page](issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- [The Odin Project](https://www.theodinproject.com/courses/javascript/lessons/library)
+
+## 📝 License
+
+This project is [MIT](./LICENSE) licensed.
--- js/index.js
+++ js/index.js
@@ -1,77 +1,72 @@
-let myLibrary = [];
+const myLibrary = [];
 
-function Book(author, title, num, status){
-	this.author = author;
-	this.title = title;
-	this.num = num;
-	this.status = status;
+function Book(author, title, num, status) {
+  this.author = author;
+  this.title = title;
+  this.num = num;
+  this.status = status;
 }
 
-Book.prototype.read_status = function(){
-	let str = "";
-	if(this.status == true){
-		str += 'read'
-		return str;
-	}
-	else{
-		str += 'unread'
-		return str;
-	}
+Book.prototype.read_status = function () {
+  let str = '';
+  if (this.status === true) {
+    str += 'read';
+    return str;
+  }
+
+  str += 'unread';
+  return str;
 };
 
-function addBookToLibrary(book){
-	myLibrary.push(book)
+function addBookToLibrary(book) {
+  myLibrary.push(book);
 }
 
-function displayBook(myLibrary){
-	
-	const newBook = document.getElementById('tableBody');
-	let str = "";
-	myLibrary.forEach((bokk, index) => {
-		str += `
+function displayBook(myLibrary) {
+  const newBook = document.getElementById('tableBody');
+  let str = '';
+  myLibrary.forEach((bokk, index) => {
+  	str += `
 		<tr>
-		<td> ${index + 1}</td>
-		<td> ${bokk.author}</td>
-		<td> ${bokk.title}</td>
-		<td> ${bokk.num}</td>
-		<td> <input onclick="change(this, ${index})" type="button" value="${bokk.status}" id="" class="btn btn-primary"></input></td>
-		<td> <button onclick="deleted(${index})" class="btn btn-danger">Delete</button></td>
+			<td> ${index + 1}</td>
+			<td> ${bokk.author}</td>
+			<td> ${bokk.title}</td>
+			<td> ${bokk.num}</td>
+			<td> <input onclick="change(this, ${index})" type="button" value="${bokk.status}" id="" class="btn btn-primary"></input></td>
+			<td> <button onclick="deleted(${index})" class="btn btn-danger">Delete</button></td>
 		</tr>
-		`
-	});
-	tableBody.innerHTML = str;
+		`;
+  });
+  newBook.innerHTML = str;
 }
 
 function change(elem, index) {
-	// var elem = document.getElementById("myButton1");
-	if (elem.value =="read") {
-		elem.value = "unread";
-	}	else {
-		elem.value = "read";
-	}
-	myLibrary[index].status = elem.value;
+  if (elem.value === 'read') {
+    elem.value = 'unread';
+  }	else {
+    elem.value = 'read';
+  }
+  myLibrary[index].status = elem.value;
 }
 
-function deleted(index){
-	myLibrary.splice(index, 1);
-	displayBook(myLibrary);
+function deleted(index) {
+  myLibrary.splice(index, 1);
+  displayBook(myLibrary);
 }
 
 
 const btn = document.getElementById('click');
-if(btn){
-	btn.addEventListener('click', () => {
-		const author = document.getElementById("author").value;
-		const title = document.getElementById("title").value;
-		const num = document.getElementById("num").value;
-		let status;
-		if(document.getElementById("read").checked)
-			status = true;
-		else
-			status = false;
-		let book = new Book(author, title, num, status);
-		book.status = book.read_status();
-		addBookToLibrary(book);
-		displayBook(myLibrary);
-	});
+if (btn) {
+  btn.addEventListener('click', () => {
+    const author = document.getElementById('author').value;
+    const title = document.getElementById('title').value;
+    const num = document.getElementById('num').value;
+    let status;
+    if (document.getElementById('read').checked) status = true;
+    else status = false;
+    const book = new Book(author, title, num, status);
+    book.status = book.read_status();
+    addBookToLibrary(book);
+    displayBook(myLibrary);
+  });
 }
